e01af92b5df8f1c053f193bc3765c5c8f23db7af
NitpreetA <98350822+NitpreetA@users.noreply.github.com>
2023-10-07 17:03:19 -0400

    Home page is finished

--- app/src/main/java/com/project/freshtomatoes/ui/MainActivity.kt
+++ app/src/main/java/com/project/freshtomatoes/ui/MainActivity.kt
@@ -25,7 +25,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AppTheme(useDarkTheme = false
+            AppTheme(useDarkTheme = true
             ) {
                 val navController = rememberNavController()
                 CompositionLocalProvider(LocalNavController provides navController) {
--- app/src/main/java/com/project/freshtomatoes/ui/layout/TopBar.kt
+++ app/src/main/java/com/project/freshtomatoes/ui/layout/TopBar.kt
@@ -10,12 +10,14 @@ import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.unit.em
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
  fun TopTomatoBar() {
 
-        TopAppBar(title = { Text(text = "FreshTomatos") }, colors = TopAppBarDefaults.smallTopAppBarColors(
+        TopAppBar(title = { Text(text = "FreshTomatos", fontSize = 7.em, fontStyle = FontStyle.Italic) }, colors = TopAppBarDefaults.smallTopAppBarColors(
             containerColor = MaterialTheme.colorScheme.primaryContainer,
         ),)
 
