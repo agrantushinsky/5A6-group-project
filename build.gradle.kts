e5a5703ead561006b6d1ee3bec4f985685df1eaa
NitpreetA <98350822+NitpreetA@users.noreply.github.com>
2023-10-07 21:04:42 -0400

    Fixing ktlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.1.0" apply false
@@ -5,13 +7,34 @@ plugins {
     id("org.jlleitschuh.gradle.ktlint") version "11.6.0"
 }
 
+subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint") // Version should be inherited from parent
+
+    repositories {
+        // Required to download KtLint
+        mavenCentral()
+    }
+
+    // Optionally configure plugin
+    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+        debug.set(true)
+    }
+
+}
+
+
 ktlint {
     verbose.set(true)
     outputToConsole.set(true)
     coloredOutput.set(true)
+    reporters {
+        reporter(ReporterType.CHECKSTYLE)
+        reporter(ReporterType.JSON)
+        reporter(ReporterType.HTML)
+    }
     filter {
-        exclude("/ExampleInstrumentedTest.kt")
-        exclude("/settings.gradle.kts")
-        exclude("**/ktlintKotlinScriptCheck.txt")
+        include("**/*.kt")
+        exclude("**/build/**")
     }
 }
+
