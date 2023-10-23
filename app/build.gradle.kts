02f6716b36696caf14b540f821c67156679dc82b
NitpreetA <98350822+NitpreetA@users.noreply.github.com>
2023-10-23 15:44:44 -0400

    Added google sign-in

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.navigation:navigation-compose:2.7.2")
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -69,4 +70,6 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     implementation(platform("com.google.firebase:firebase-bom:32.4.0"))
     implementation("com.google.firebase:firebase-analytics-ktx")
+    implementation("com.google.firebase:firebase-auth-ktx")
 }
+
--- app/src/main/java/com/project/freshtomatoes/ui/MainActivity.kt
+++ app/src/main/java/com/project/freshtomatoes/ui/MainActivity.kt
@@ -1,15 +1,44 @@
 package com.project.freshtomatoes
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.ManagedActivityResultLauncher
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.compose.AppTheme
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.EmailAuthCredential
+import com.google.firebase.auth.EmailAuthProvider
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+
 import com.project.freshtomatoes.ui.NavGraph
 import com.project.freshtomatoes.ui.layout.MainLayout
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 
 val LocalNavController = compositionLocalOf<NavHostController> { error("No Nav Controller") }
 
@@ -20,13 +49,64 @@ class MainActivity : ComponentActivity() {
             AppTheme(
                 useDarkTheme = true
             ) {
-                val navController = rememberNavController()
-                CompositionLocalProvider(LocalNavController provides navController) {
-                    MainLayout {
-                        NavGraph()
+                var user by remember { mutableStateOf(Firebase.auth.currentUser) }
+                val launcher = rememberFirebaseAuthLauncher(
+                    onAuthComplete = { result ->
+                        user = result.user
+                    },
+                    onAuthError = {
+                        user = null
+                    }
+                )
+                val token = "756539577939-lplb0tvvtbis5745q0835ndrr7bg3slc.apps.googleusercontent.com"
+                val context = LocalContext.current
+                Column {
+                    if (user == null) {
+                        Text("Not logged in")
+                        Button(onClick = {
+                            val gso =
+                                GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                                    .requestIdToken(token)
+                                    .requestEmail()
+                                    .build()
+                            val googleSignInClient = GoogleSignIn.getClient(context, gso)
+                            launcher.launch(googleSignInClient.signInIntent)
+                        }) {
+                            Text("Sign in via Google")
+                        }
+                    } else {
+                        Text("Welcome ${user!!.displayName}")
+                        Button(onClick = {
+                            Firebase.auth.signOut()
+                            user = null
+                        }) {
+                            Text("Sign out")
+                        }
                     }
                 }
+
             }
+        }
+    }
+}
+
+@Composable
+fun rememberFirebaseAuthLauncher(
+    onAuthComplete: (AuthResult) -> Unit,
+    onAuthError: (ApiException) -> Unit
+): ManagedActivityResultLauncher<Intent, ActivityResult> {
+    val scope = rememberCoroutineScope()
+    return rememberLauncherForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+        try {
+            val account = task.getResult(ApiException::class.java)!!
+            val credential = GoogleAuthProvider.getCredential(account.idToken!!, null)
+            scope.launch {
+                val authResult = Firebase.auth.signInWithCredential(credential).await()
+                onAuthComplete(authResult)
+            }
+        } catch (e: ApiException) {
+            onAuthError(e)
         }
     }
 }
