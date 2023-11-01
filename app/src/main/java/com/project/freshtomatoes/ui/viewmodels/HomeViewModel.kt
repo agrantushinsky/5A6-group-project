bd03aec7ed3cbad7fb3cb18638aca378305b9b2a
2183276 <a@amazon.com>
2023-11-01 14:09:47 -0400

    Viewmodel still not working. Removed the list.

--- app/src/main/java/com/project/freshtomatoes/ui/viewmodels/HomeViewModel.kt
+++ app/src/main/java/com/project/freshtomatoes/ui/viewmodels/HomeViewModel.kt
@@ -1,43 +1,45 @@
 package com.project.freshtomatoes.ui.viewmodels
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.project.freshtomatoes.data.Movie
 import com.project.freshtomatoes.data.TmdbRequest
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 data class MovieDisplay(
     val label: String,
     var movieList: List<Movie> = emptyList(),
-    val populateMovies: suspend () -> List<Movie>,
+    val populateMovies: suspend (MovieDisplay) -> Unit,
 )
 
 class HomeViewModel : ViewModel() {
     private val _requester = TmdbRequest()
 
-    private val _displays = MutableStateFlow(listOf(
-        MovieDisplay("Popular Movies") {
-            val response = _requester.popularMovies()
-            return@MovieDisplay response.results
-        },
-        MovieDisplay("New Movies") {
-            val response = _requester.nowPlayingMovies()
-            return@MovieDisplay response.results
-        },
-    ))
+    private val popularMovies = mutableStateOf(MovieDisplay("Popular Movies") {
+        val response = _requester.popularMovies()
+        it.movieList = response.results
+    })
 
-    val displays = _displays.asStateFlow()
+    private val newMovies = mutableStateOf(MovieDisplay("New Movies") {
+        val response = _requester.nowPlayingMovies()
+        it.movieList = response.results
+    })
 
     init {
-        _displays.value.forEachIndexed { index, display ->
-            viewModelScope.launch(Dispatchers.IO) {
-                _displays.collect {
-                    _displays.value[index].movieList = display.populateMovies()
-                }
-            }
+        viewModelScope.launch(Dispatchers.IO) {
+            popularMovies.value.populateMovies(popularMovies.value)
+            newMovies.value.populateMovies(newMovies.value)
         }
+    }
+
+    fun getMovieDisplay(): List<MovieDisplay> {
+        return listOf(popularMovies.value, newMovies.value)
     }
 }
