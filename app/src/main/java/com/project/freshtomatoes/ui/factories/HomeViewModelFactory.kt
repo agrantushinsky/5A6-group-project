9cd47a65265d1ea06cd8bf54ee82d50baf26ef63
NitpreetA <98350822+NitpreetA@users.noreply.github.com>
2023-11-09 15:36:22 -0500

    changed bottom bar

--- app/src/main/java/com/project/freshtomatoes/ui/factories/HomeViewModelFactory.kt
+++ app/src/main/java/com/project/freshtomatoes/ui/factories/HomeViewModelFactory.kt
@@ -1,8 +1,9 @@
 package com.project.freshtomatoes.ui.factories
 
+import HomeViewModel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.project.freshtomatoes.ui.viewmodels.HomeViewModel
+
 
 class HomeViewModelFactory : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
