0b9170eed7f26c8615c720d7789d903e9d5b3cb6
agrantushinsky <aidanushinsky@gmail.com>
2023-11-22 17:00:27 -0500

    Add movie details caching

--- app/src/main/java/com/project/freshtomatoes/ui/FreshTomatoes.kt
+++ app/src/main/java/com/project/freshtomatoes/ui/FreshTomatoes.kt
@@ -4,16 +4,19 @@ import android.app.Application
 import com.google.firebase.Firebase
 import com.google.firebase.auth.auth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.project.freshtomatoes.data.Movie
 
 class FreshTomatoes : Application() {
     /* Always be able to access the module ("static") */
     companion object {
         lateinit var appModule: AppModule
+        lateinit var moviesById: HashMap<Int, Movie>
     }
 
     /* Called only once at beginning of application's lifetime */
     override fun onCreate() {
         super.onCreate()
         appModule = AppModule(this, Firebase.auth, FirebaseFirestore.getInstance())
+        moviesById = HashMap()
     }
 }
