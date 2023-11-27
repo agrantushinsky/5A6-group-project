e094f8b78fe690572ec84e0de923ae070ccc958a
Josed0912 <josephdtowers2@gmail.com>
2023-11-27 15:46:02 -0500

    Added password visibility

--- app/src/main/java/com/project/freshtomatoes/ui/pages/EmailPassword/AuthLoginScreen.kt
+++ app/src/main/java/com/project/freshtomatoes/ui/pages/EmailPassword/AuthLoginScreen.kt
@@ -5,8 +5,11 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -19,11 +22,16 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.project.freshtomatoes.LocalNavController
+import com.project.freshtomatoes.R
 import com.project.freshtomatoes.ui.Router
 import com.project.freshtomatoes.ui.factories.AuthViewModelFactory
 
@@ -41,6 +49,10 @@ fun AuthLoginScreen(
     var password by rememberSaveable {
         mutableStateOf("")
     }
+
+    var passwordVisibility by rememberSaveable {
+        mutableStateOf(false)
+    }
     var signedInSuccess = rememberSaveable {
         mutableStateOf(false)
     }
@@ -52,6 +64,12 @@ fun AuthLoginScreen(
         mutableStateOf(false)
     }
 
+    val icon = if(passwordVisibility)
+        painterResource(id = R.drawable.design_ic_visibility)
+    else
+        painterResource(id = R.drawable.design_ic_visibility_off)
+    
+
     Column(
         modifier = Modifier
             .padding(20.dp)
@@ -60,7 +78,24 @@ fun AuthLoginScreen(
     ) {
         Text("Log In", fontSize = 32.sp, fontWeight = FontWeight.Bold, modifier = Modifier.padding(20.dp))
         TextField(value = email, onValueChange = { email = it }, label = { Text("Email") }, modifier = Modifier.padding(20.dp))
-        TextField(value = password, onValueChange = { password = it }, label = { Text("Password") }, modifier = Modifier.padding(20.dp))
+        TextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Password") },
+            trailingIcon = {
+                IconButton(onClick = { passwordVisibility = !passwordVisibility }) {
+                    Icon(
+                        painter = icon,
+                        contentDescription = "Visibility Icon"
+                    )
+                }
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Password
+            ),
+            visualTransformation = if(passwordVisibility) VisualTransformation.None
+                    else PasswordVisualTransformation(),
+            modifier = Modifier.padding(20.dp))
 
         Row(horizontalArrangement = Arrangement.SpaceEvenly, modifier = Modifier.fillMaxWidth()) {
             Button(onClick = {
--- app/src/main/java/com/project/freshtomatoes/ui/pages/EmailPassword/AuthSignUpScreen.kt
+++ app/src/main/java/com/project/freshtomatoes/ui/pages/EmailPassword/AuthSignUpScreen.kt
@@ -7,8 +7,11 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -19,11 +22,16 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.project.freshtomatoes.LocalNavController
+import com.project.freshtomatoes.R
 import com.project.freshtomatoes.ui.Router
 import com.project.freshtomatoes.ui.factories.AuthViewModelFactory
 
@@ -42,6 +50,25 @@ fun AuthSignUpScreen(authViewModel: AuthViewModel = viewModel(factory = AuthView
         mutableStateOf("")
     }
 
+    var passwordVisibility by rememberSaveable {
+        mutableStateOf(false)
+    }
+
+    val iconP = if(passwordVisibility)
+        painterResource(id = R.drawable.design_ic_visibility)
+    else
+        painterResource(id = R.drawable.design_ic_visibility_off)
+
+
+    var confirmPasswordVisibility by rememberSaveable {
+        mutableStateOf(false)
+    }
+
+    val iconCP = if(confirmPasswordVisibility)
+        painterResource(id = R.drawable.design_ic_visibility)
+    else
+        painterResource(id = R.drawable.design_ic_visibility_off)
+
     Column(
         modifier = Modifier
             .padding(20.dp)
@@ -57,8 +84,39 @@ fun AuthSignUpScreen(authViewModel: AuthViewModel = viewModel(factory = AuthView
 
         Text("Sign Up", fontSize = 32.sp, fontWeight = FontWeight.Bold, modifier = Modifier.padding(20.dp))
         TextField(value = email, onValueChange = { email = it }, label = { Text("Email") }, modifier = Modifier.padding(20.dp))
-        TextField(value = password, onValueChange = { password = it }, label = { Text("Password") }, modifier = Modifier.padding(20.dp))
-        TextField(value = confirmPassword, onValueChange = { confirmPassword = it }, label = { Text("Confirm Password") }, modifier = Modifier.padding(20.dp))
+        TextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Password") },
+            trailingIcon = {
+                IconButton(onClick = { passwordVisibility = !passwordVisibility }) {
+                    Icon(
+                        painter = iconP,
+                        contentDescription = "Visibility Icon"
+                    )
+                }
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Password
+            ),
+            visualTransformation = if(passwordVisibility) VisualTransformation.None
+            else PasswordVisualTransformation(),
+            modifier = Modifier.padding(20.dp))
+        TextField(
+            value = confirmPassword,
+            onValueChange = { confirmPassword = it },
+            label = { Text("Confirm Password") },
+            trailingIcon = {
+                IconButton(onClick = { confirmPasswordVisibility = !confirmPasswordVisibility }) {
+                    Icon(
+                        painter = iconCP,
+                        contentDescription = "Visibility Icon"
+                    )
+                }
+            },
+            visualTransformation = if(confirmPasswordVisibility) VisualTransformation.None
+            else PasswordVisualTransformation(),
+            modifier = Modifier.padding(20.dp))
         Spacer(modifier = Modifier.height(10.dp))
 
         Row(horizontalArrangement = Arrangement.SpaceEvenly, modifier = Modifier.fillMaxWidth()) {
