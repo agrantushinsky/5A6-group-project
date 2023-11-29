f5f677b9b489de3e0dae6f2056fd8f80b9b655e3
NitpreetA <98350822+NitpreetA@users.noreply.github.com>
2023-11-29 14:24:56 -0500

    buttons added

--- app/src/main/java/com/project/freshtomatoes/ui/components/MovieReviewCard.kt
+++ app/src/main/java/com/project/freshtomatoes/ui/components/MovieReviewCard.kt
@@ -5,10 +5,16 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Create
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
@@ -16,7 +22,10 @@ import com.project.freshtomatoes.data.MovieReview
 
 @Composable
 fun MovieReviewCard(movieReview: MovieReview) {
-    Card(modifier = Modifier.height(100.dp).padding(10.dp).fillMaxWidth()) {
+    Card(modifier = Modifier
+        .height(100.dp)
+        .padding(10.dp)
+        .fillMaxWidth()) {
         Row {
             Column {
                 Card {
@@ -31,6 +40,16 @@ fun MovieReviewCard(movieReview: MovieReview) {
                 Text("${movieReview.movie.title}", fontWeight = FontWeight.Bold)
                 Text("Rating: ${movieReview.review.rating}/5")
                 Text("Comments: ${movieReview.review.review}")
+
+            }
+            Row(modifier = Modifier.weight(1f)) {
+                Button(onClick = { /*TODO*/ }) {
+
+                    Icon(imageVector = Icons.Filled.Create, contentDescription =  "edit")
+                }
+                Button(onClick = { /*TODO*/ }) {
+                    Icon(imageVector = Icons.Filled.Delete,contentDescription =  "edit" )
+                }
             }
         }
     }
