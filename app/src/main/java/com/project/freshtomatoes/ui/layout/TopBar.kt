efe431a9930c4866f461dc83bd5c5c74fa0fb26b
agrantushinsky <aidanushinsky@gmail.com>
2023-12-05 21:02:55 -0500

    Better navigation with back buttons

--- app/src/main/java/com/project/freshtomatoes/ui/layout/TopBar.kt
+++ app/src/main/java/com/project/freshtomatoes/ui/layout/TopBar.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -14,6 +15,7 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
@@ -21,6 +23,18 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import com.project.freshtomatoes.LocalNavController
 import com.project.freshtomatoes.ui.Router
 import com.project.freshtomatoes.ui.factories.AuthViewModelFactory
+
+// Show back buttons on page that aren't "1 click" navigable.
+private fun shouldShowBackButton(route: String?): Boolean {
+    if(route == null)
+        return false
+
+    return !(route == Router.Home.route ||
+        route == Router.YourReviews.route ||
+        route == Router.About.route ||
+        route == Router.Info.route ||
+        route == Router.Profile.route)
+}
 
 /**
  * TopBar to be displayed at the top of the screen. Shows FreshTomatoes header and login status.
@@ -34,11 +48,19 @@ import com.project.freshtomatoes.ui.factories.AuthViewModelFactory
 fun TopBar(authViewModel: AuthViewModel = viewModel(factory = AuthViewModelFactory())) {
     val navController = LocalNavController.current
     val userState = authViewModel.currentUser().collectAsState()
+    val backStack = navController.currentBackStack.collectAsState()
 
     TopAppBar(
         title =
         {
-            Row(horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxWidth()) {
+            Row(horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+                if (shouldShowBackButton(backStack.value.lastOrNull()?.destination?.route)) {
+                    IconButton(onClick = {
+                        navController.popBackStack()
+                    }) {
+                        Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "Back button")
+                    }
+                }
                 Text(text = "FreshTomatoes")
                 if (userState.value != null) {
                     Text(text = "${userState.value?.email?.split("@")?.get(0)}", fontSize = 18.sp, fontWeight = FontWeight.Bold)
